ab49891cfc8c98739cf2cd606f2dc3e1fe39c4f6
Parag Dalvi <dalviparag76@gmail.com>
2020-10-22 22:59:07 +0530

    little in link sharing page

--- src/components/Quizes/LinkSharingComponent.js
+++ src/components/Quizes/LinkSharingComponent.js
@@ -1,15 +1,29 @@
 import React from 'react';
-import { Card, Container } from 'reactstrap';
+import { Button, Card, CardBody, Col, Container, Input, Row } from 'reactstrap';
 import CustomNavbar from '../Reuse/CustomNavbar';
+import { FaCopy, FaWhatsapp } from 'react-icons/fa';
 
 const LinkSharing = ({ name, user }) => {
     return (
         <div>
             <CustomNavbar />
             <Container>
+                <br></br>
                 <Card>
+                    <CardBody>
+                        <p>/{name}-{user.uid}</p>
+                        <p>Thanks for saving your answers, now share the below link with your beloved people to see who knows you better.</p>
 
-                    <p>/{name}-{user.uid}</p>
+                        <div style={{ display: 'flex' }}>
+                            <Input style={{ flex: '1' }} type="text" disabled value={'foo-quiz.web.app/answer/' + name + '-' + user.uid} />
+                            <Button color="primary" className="ml-1"><FaCopy /></Button>
+                        </div>
+
+                        <br></br>
+                        <Button color="success"><FaWhatsapp /></Button>
+
+
+                    </CardBody>
                 </Card>
             </Container>
         </div>
--- src/components/Reuse/CustomNavbar.js
+++ src/components/Reuse/CustomNavbar.js
@@ -9,18 +9,17 @@ import {
   NavItem,
   NavLink, Button
 } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 const CustomNavbar = ({ toggleModal }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => setIsOpen(!isOpen);
 
-  const history = useHistory();
-
   return (
     <div>
       <Navbar color="light" light expand="md">
-        <NavbarBrand onClick={() => history.push('/')}>FOO-Quiz</NavbarBrand>
+        <NavbarBrand><Link to="/" style={{ color: 'inherit', textDecoration: 'inherit' }}>FOO-Quiz</Link></NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="mr-auto" navbar>
