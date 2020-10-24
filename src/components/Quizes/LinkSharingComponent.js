e7aa0551ddd06c2a0ecb4a8cac4f265c9a81deb9
Parag Dalvi <dalviparag76@gmail.com>
2020-10-25 00:27:25 +0530

    link sharing simple

--- src/components/Quizes/LinkSharingComponent.js
+++ src/components/Quizes/LinkSharingComponent.js
@@ -4,6 +4,9 @@ import CustomNavbar from '../Reuse/CustomNavbar';
 import { FaCopy, FaWhatsapp } from 'react-icons/fa';
 
 const LinkSharing = ({ name, user }) => {
+
+    const copyValue = 'Hey there, I challenge you to take this test. This test has a few simple questions, your task is to guess what my answer would have to the same question. This way I will know, how well you know me? Follow this link to take the quiz foo-quiz.web.app/answer/' + name + '-' + user.uid;
+
     return (
         <div>
             <CustomNavbar />
@@ -11,17 +14,25 @@ const LinkSharing = ({ name, user }) => {
                 <br></br>
                 <Card>
                     <CardBody>
-                        <p>/{name}-{user.uid}</p>
-                        <p>Thanks for saving your answers, now share the below link with your beloved people to see who knows you better.</p>
-
+                        <p>Thanks for saving your answers, your answers are saved in our database. Now share the below link with your beloved people to see who knows you better.</p>
                         <div style={{ display: 'flex' }}>
-                            <Input style={{ flex: '1' }} type="text" disabled value={'foo-quiz.web.app/answer/' + name + '-' + user.uid} />
-                            <Button color="primary" className="ml-1"><FaCopy /></Button>
+                            <Input style={{ flex: '1' }} type="textarea" disabled value={copyValue} />
+                            <Button color="primary" onClick={() => { navigator.clipboard.writeText(copyValue) }} className="ml-1"><FaCopy /></Button>
                         </div>
 
                         <br></br>
-                        <Button color="success"><FaWhatsapp /></Button>
 
+                        <div className="d-flex justify-content-center mb-4">
+                            <Button color="success" onClick={() => window.open('https://wa.me/?text=' + copyValue)}><FaWhatsapp /></Button>
+                            <Button className="ml-1" color="primary" onClick={() => { navigator.clipboard.writeText(copyValue) }}><FaCopy /></Button>
+                        </div>
+
+
+                        <p>Just copy the link.</p>
+                        <div style={{ display: 'flex' }}>
+                            <Input style={{ flex: '1' }} type="text" disabled value={'foo-quiz.web.app/answer/' + name + '-' + user.uid} />
+                            <Button color="primary" className="ml-1" onClick={() => { navigator.clipboard.writeText('foo-quiz.web.app/answer/' + name + '-' + user.uid) }}><FaCopy /></Button>
+                        </div>
 
                     </CardBody>
                 </Card>
